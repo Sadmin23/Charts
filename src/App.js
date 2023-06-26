2422ee3f67c06103057b0e8e1dc4bbb5b5f4c6ea
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2023-06-26 23:44:20 +0600

    data used

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import Chart from "chart.js/auto";
 import { CategoryScale } from "chart.js";
-import { useState } from "react";
 import { BarChart } from "./BarChart";
+import { chartdata } from "./Data";
 
 Chart.register(CategoryScale);
  
 export default function App() {
-  const chartData = {
-    labels: ['Red', 'Orange', 'Blue'],
+  const userData = {
+    labels: chartdata.data.map(data=>data.name),
     // datasets is an array of objects where each object represents a set of data to display corresponding to the labels above. for brevity, we'll keep it at one object
     datasets: [
         {
-          data: [55, 23, 96],
+          data: chartdata.data.map(data=>data.value),
           // you can set indiviual colors for each bar
-          backgroundColor: [
-            "#50AF95",
-            "#f3ba2f",
-            "#2a71d0"
-          ],
+          backgroundColor: chartdata.data.map(data=>data.color),
           borderColor: "black",
         }
     ]
@@ -25,8 +21,8 @@ export default function App() {
  
   return (
     <div className="App">
-      <BarChart chartData={chartData} />
-      <BarChart chartData={chartData} />
+      <BarChart chartData={userData} />
+      <BarChart chartData={userData} />
     </div>
   );
   }
