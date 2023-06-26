e49c9f83777c2c5a79830014eefa9efdc8194b63
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2023-06-26 22:33:15 +0600

    failed barchart

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,18 @@
-import React from 'react';
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend,
-} from 'chart.js';
-import { Bar } from 'react-chartjs-2';
-import { faker } from '@faker-js/faker';
+import BarChart from "./BarChart.tsx"
+import { UserData } from "./Data.ts"
 
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend
-);
+export default function App() {
 
-export const options = {
-  responsive: true,
-  plugins: {
-    legend: {
-      position: 'top' as const,
-    },
-    title: {
-      display: true,
-      text: 'Chart.js Bar Chart',
-    },
-  },
-};
+  const chartData = {
+    labels: UserData.data.map((data)=>data.name),
+    datasets: [{
+      data: UserData.data.map((data)=>data.value)
+    }]
+  }
 
-const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-
-export const data = {
-  labels,
-  datasets: [
-    {
-      label: 'Dataset 1',
-      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
-      backgroundColor: 'rgba(255, 99, 132, 0.5)',
-    },
-    {
-      label: 'Dataset 2',
-      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
-      backgroundColor: 'rgba(53, 162, 235, 0.5)',
-    },
-  ],
-};
-
-export function App() {
-  return <Bar options={options} data={data} />;
+  return (
+      <div>
+        <BarChart userData={chartData}/>
+      </div>
+    )
 }
---
+++ src/Data.ts
@@ -0,0 +1,19 @@
+export const UserData = { chartName: "Recall", 
+    data: [
+    {
+      name: "Random",
+      value: 0.2,
+      color: "#D1D5DB",
+    },
+    {
+      name: "Shaped",
+      value: 0.5,
+      color: "#A855F7",
+    },
+    {
+      name: "Toplist",
+      value: 0.8,
+      color: "#0EA5E9",
+    },
+  ] 
+}
