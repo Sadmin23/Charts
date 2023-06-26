342eed593e3456c538fa67001a351a1e86b9c202
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2023-06-26 23:19:15 +0600

    sample data

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import Chart from "chart.js/auto";
+import { CategoryScale } from "chart.js";
+import { useState } from "react";
+import { BarChart } from "./BarChart";
+
+Chart.register(CategoryScale);
+ 
+export default function App() {
+  const [chartData, setChartData] = useState({
+    labels: ['Red', 'Orange', 'Blue'],
+    // datasets is an array of objects where each object represents a set of data to display corresponding to the labels above. for brevity, we'll keep it at one object
+    datasets: [
+        {
+          label: 'Popularity of colours',
+          data: [55, 23, 96],
+          // you can set indiviual colors for each bar
+          // backgroundColor: [
+          //   'rgba(255, 255, 255, 0.6)'
+          //   'rgba(255, 255, 255, 0.6)'
+          //   'rgba(255, 255, 255, 0.6)'
+          // ],
+          borderWidth: 1,
+        }
+    ]
+  });
+ 
+  return (
+    <div className="App">
+      <BarChart chartData={chartData} />
+      <BarChart chartData={chartData} />
+    </div>
+  );
+  }
