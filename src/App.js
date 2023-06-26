a2f0b66b7a19d781118c9743f082bae9a2ebcbcc
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2023-06-26 23:36:16 +0600

    UI fixed

--- src/App.js
+++ src/App.js
@@ -6,23 +6,22 @@ import { BarChart } from "./BarChart";
 Chart.register(CategoryScale);
  
 export default function App() {
-  const [chartData, setChartData] = useState({
+  const chartData = {
     labels: ['Red', 'Orange', 'Blue'],
     // datasets is an array of objects where each object represents a set of data to display corresponding to the labels above. for brevity, we'll keep it at one object
     datasets: [
         {
-          label: 'Popularity of colours',
           data: [55, 23, 96],
           // you can set indiviual colors for each bar
-          // backgroundColor: [
-          //   'rgba(255, 255, 255, 0.6)'
-          //   'rgba(255, 255, 255, 0.6)'
-          //   'rgba(255, 255, 255, 0.6)'
-          // ],
-          borderWidth: 1,
+          backgroundColor: [
+            "#50AF95",
+            "#f3ba2f",
+            "#2a71d0"
+          ],
+          borderColor: "black",
         }
     ]
-  });
+  };
  
   return (
     <div className="App">
