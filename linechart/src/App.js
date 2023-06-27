4f57b4f836215bde54920f0620f609a7ab3f196f
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2023-06-27 11:09:10 +0600

    Line Chart rendered

--- linechart/src/App.js
+++ linechart/src/App.js
@@ -1,60 +1,35 @@
-import React from 'react';
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend,
-} from 'chart.js';
-import { Line } from 'react-chartjs-2';
+import Chart from "chart.js/auto";
+import { CategoryScale } from "chart.js";
+import LineChart from "./LineChart";
 import { faker } from '@faker-js/faker';
 
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend
-);
-
-export const options = {
-  responsive: true,
-  plugins: {
-    // legend: {
-    //   position: 'top' as const,
-    // },
-    title: {
-      display: true,
-      text: 'Chart.js Line Chart',
-    },
-  },
-};
+Chart.register(CategoryScale);
 
 const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+ 
+export default function App() {
 
-export const data = {
-  labels,
-  datasets: [
-    {
-      label: 'Dataset 1',
-      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-      borderColor: 'rgb(255, 99, 132)',
-      backgroundColor: 'rgba(255, 99, 132, 0.5)',
-    },
-    {
-      label: 'Dataset 2',
-      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-      borderColor: 'rgb(53, 162, 235)',
-      backgroundColor: 'rgba(53, 162, 235, 0.5)',
-    },
-  ],
-};
-
-export function App() {
-  return <Line options={options} data={data} />;
+  const chartData = {
+    labels,
+    datasets: [
+      {
+        label: 'Dataset 1',
+        data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+        borderColor: 'rgb(255, 99, 132)',
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      },
+      {
+        label: 'Dataset 2',
+        data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+        borderColor: 'rgb(53, 162, 235)',
+        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+      },
+    ],
+  };
+ 
+  return (
+    <div className="App">
+      <LineChart chartData={chartData} />
+    </div>
+  );
 }
---
+++ linechart/src/Data.js
@@ -0,0 +1,43 @@
+export const lines = [
+  {
+    title: "Personalized",
+    color: "#8B5CF6",
+    data: [
+      {
+        name: "06/04", //x axis value
+        val: 4000, //y axis value
+      },
+      {
+        name: "06/03",
+        val: 3000,
+      },
+      {
+        name: "06/02",
+        val: 2000,
+      },
+      {
+        name: "06/01",
+        val: 2780,
+      },
+    ],
+  },
+  { title: "Toplist", color: "#22C55E",
+  data: [
+    {
+      name: "06/04", //x axis value
+      val: 4000, //Y axis value
+    },
+    {
+      name: "06/03",
+      val: 3000,
+    },
+    {
+      name: "06/02",
+      val: 2000,
+    },
+    {
+      name: "06/01",
+      val: 2780,
+    },
+  ] },
+];
